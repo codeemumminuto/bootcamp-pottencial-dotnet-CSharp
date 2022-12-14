1e93aac74a935b53fe70880d4d43e8db604d31ec
Lucas A <120221005+codeemumminuto@users.noreply.github.com>
2022-12-14 15:42:47 -0300

    abstração encapsulamento herança e polimorfismo
    Novos assuntos aprendidos: abstração encapsulamento herança e polimorfismo

---
+++ abstracao-encapsulamento-heranca-e-polimorfismo/Program.cs
@@ -0,0 +1,48 @@
+﻿using abstracao_pratica.Models;
+
+/*
+Abstração
+Pessoa p1 = new Pessoa();
+p1.Nome = "Lucas";
+p1.Idade = 23;
+p1.Apresentar();
+*/
+
+/*
+Encapsulamento
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.Sacar(200);
+c1.ExibirSaldo();
+*/
+
+/*
+Herança
+Aluno a1 = new Aluno();
+a1.Nome = "Lucas";
+a1.Idade = 23;
+a1.Email = "lll@gmail.com";
+a1.Nota = 7.5;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Prof";
+p1.Idade = 43;
+p1.Email = "ppp@gmail.com";
+p1.Salario = 2000.00M;
+p1.Apresentar();
+*/
+
+//Polmorfismo
+Aluno a1 = new Aluno();
+a1.Nome = "Lucas";
+a1.Idade = 23;
+a1.Email = "lll@gmail.com";
+a1.Nota = 7.5;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Prof";
+p1.Idade = 43;
+p1.Email = "ppp@gmail.com";
+p1.Salario = 2000.00M;
+p1.Apresentar();
