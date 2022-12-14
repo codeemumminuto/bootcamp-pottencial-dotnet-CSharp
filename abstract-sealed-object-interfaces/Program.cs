089bd3eedc277de1acb123b4120afa6dc692605f
Lucas A <120221005+codeemumminuto@users.noreply.github.com>
2022-12-14 16:28:58 -0300

    abstract-sealed-object-interfaces
    Novos assuntos: abstract-sealed-object-interfaces

---
+++ abstract-sealed-object-interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using abstrata.Models;
+using abstrata.interfaces;
+
+Corrente c1 = new Corrente();
+c1.Creditar(500);
+c1.ExibirSaldo();
+
+
+//construtor por herança, ao exigir um parametro obrigatorio (construtor) na classe mãe, é necessário adicionar um construtor na classe filha junto com base(nome)
+//public exemplo(string nome) : base(nome)
+
+//public sealed sela uma classe ou um método e proibe outras classes de herdarem(classes) ou usarem(métodos)
+
+
+//Classe object topo da hierarquia - serviços de baixo nível
+
+//Interface
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Dividir(2,1));
